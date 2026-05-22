d50a3ac3f8b31617ade6fbdf7baca4e271865628
vijaynvb <vijay.nvb@gmail.com>
2026-05-22 16:23:24 +0530

    Add initial implementation of positive number functions and array filtering

---
+++ 03_Arrow_Funtions/01_Arrow_function.js
@@ -0,0 +1,26 @@
+function isPositive(num){
+    if(num> 0)
+        return true;
+    return false;
+}
+
+annanomusUnc = function(num){
+    if(num> 0)
+        return true;
+    return false;
+}
+
+arrowFunction = (num)=>{
+    if(num> 0)
+        return true;
+    return false;
+}
+arrowFunction1 = num => num > 0
+
+const sum =(a,b,c) =>  (a+b) * c
+
+const numbers = [100,-100,200,-200]
+
+const positiveArray = numbers.filter(num => num > 0) // [100,200]
+
+console.log(positiveArray)
